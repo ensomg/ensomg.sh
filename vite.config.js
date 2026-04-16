e35722e70cda4de555bcf3e2b4c292274433437f
ens69420 <isikenes@proton.me>
2026-04-17 01:02:40 +0300

    fix: enforce discord verification with strict LF encoding and vercel rewrite

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,23 @@ function versionPlugin() {
   }
 }
 
+function discordPlugin() {
+  return {
+    name: 'discord-plugin',
+    generateBundle() {
+      this.emitFile({
+        type: 'asset',
+        fileName: '.well-known/discord',
+        source: `dh=067e705497fcc6d2817d3d9326c3c8defa766d50
+dh=3f47de1af92af8152112b460b5987520007a42a0
+dh=708d872d7307e55548f81d1aabfb211d64b5ee54
+dh=fbe0feb01947bcc6d0be040fc0ca7f4395f52c36\n`
+      });
+    }
+  }
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), mapPlugin(), versionPlugin()],
+  plugins: [react(), tailwindcss(), mapPlugin(), versionPlugin(), discordPlugin()],
 })
